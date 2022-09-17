3d872cd06dd44a1580dc84076c2bcdabb53d569d
Nishant Patel <patelnishant@gmail.com>
2022-09-17 21:29:00 +1000

    replace title

--- variableusername.py
+++ variableusername.py
@@ -10,7 +10,16 @@ def home():
 def orders(user_name, order_id):
     return f'<p>Fetching order #{order_id} for {user_name}.</p>'
 
+
+@app.route('/article/<article_name>')
+def article(article_name):
+  return f'''
+  <h2>{article_name.replace('-', ' ').title()}</h2>
+  <a href='/'>Return back to home page</a>
+  '''
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
-
+#URL - htt
 #URL - http://192.168.1.245:8080/order/nishant/12
