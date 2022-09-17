c8f509ff211103a0151493f88226951aa7e229e0
Nishant Patel <patelnishant@gmail.com>
2022-09-17 19:19:05 +1000

    webapp

---
+++ web.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+
+def home():
+    return '''
+    <h1>Hello, World!</h1>
+    <p>My first paragraph.</p>
+    <a href="https://www.codecademy.com">CODECADEMY</a>
+    '''
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
