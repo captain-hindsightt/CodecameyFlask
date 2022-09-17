6ec2a07723eb7e949ccebed647b3d98066ad2894
captain-hindsightt <52739696+captain-hindsightt@users.noreply.github.com>
2022-09-17 21:30:52 +1000

    udpated url

--- variableusername.py
+++ variableusername.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 def home():
     return '<h1>Hello, World!</h1>'
 
+
 @app.route('/order/<user_name>/<int:order_id>')
 def orders(user_name, order_id):
     return f'<p>Fetching order #{order_id} for {user_name}.</p>'
+
+#URL - http://192.168.1.245:8080/order/nishant/12
 
 
 @app.route('/article/<article_name>')
@@ -18,8 +21,8 @@ def article(article_name):
   <a href='/'>Return back to home page</a>
   '''
 
+#URL - http://192.168.1.245:8080/article/duck-init-real
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
-#URL - htt
-#URL - http://192.168.1.245:8080/order/nishant/12
